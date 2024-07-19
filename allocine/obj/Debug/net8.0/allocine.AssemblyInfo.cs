85f13bc476317eca85abef6dd3da4976feb359a3
DialloHabib77 <justcodeur88@gmail.com>
2024-07-19 15:26:07 +0000

    Update CanalPlus and Series

--- allocine/obj/Debug/net8.0/allocine.AssemblyInfo.cs
+++ allocine/obj/Debug/net8.0/allocine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("allocine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14263e01c6f606618679c3e581fb7e2bd7244602")]
 [assembly: System.Reflection.AssemblyProductAttribute("allocine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("allocine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
